ffd0e32bf07f835ae084e509a3cb6824d98d5f13
Dibyajyoti Sarkar <46948111+djsarkar93@users.noreply.github.com>
2023-10-27 19:02:26 +0530

    Quick sort algorithm

---
+++ DSA/Striver's A-Z DSA Course/[002] Sorting Techniques/005_quick_sort.py
@@ -0,0 +1,51 @@
+"""
+Quick Sort
+########################################################################################################################
+A Divide & Conquer algorithm
+########################################################################################################################
+Unstable Sort
+########################################################################################################################
+Space Complexity:
+    Best:       O( log n )
+    Average:    O( log n )
+    Worst:      O( n )
+########################################################################################################################
+Time Complexity:
+    Best:       O( n log n )
+    Average:    O( n log n )
+    Worst:      O( n^2 )
+"""
+import os
+
+
+def partition(arr, start, end):
+    pivot = arr[end]                                # Choosing the last element as pivot
+    i = start - 1
+
+    for j in range(start, end):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i + 1], arr[end] = arr[end], arr[i + 1]     # Place the pivot correctly
+    return i + 1
+
+
+def quick_sort(arr, start, end):
+    if start >= end:                                # Further divide not possible
+        return
+    
+    pivot_idx = partition(arr, start, end)          # Partition the array
+
+    quick_sort(arr, start, pivot_idx - 1)           # Before pivot
+    quick_sort(arr, pivot_idx + 1, end)             # After pivot
+
+
+if __name__ == '__main__':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    arr = [5,3,5,8,7,2]
+    print(f'Original = {arr}')
+
+    quick_sort(arr, 0, len(arr)-1)
+    print(f'Ascending = {arr}')
